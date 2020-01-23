6194b3f12f41a28b93777965efda61c5b6a920e2
LuisGC01 <luis_garciacervantes@yahoo.com.mx>
2020-01-23 17:46:37 -0600

    Se termino el ejemplo de polimorfismo

--- proyecto3/src/Herencia/Principal.java
+++ proyecto3/src/Herencia/Principal.java
@@ -13,11 +13,12 @@ import javax.swing.JOptionPane;
  */
 public class Principal {
     public static void main(String[] args) {
+        FiguraGeometrica f;
         Circulo circulo1 = new Circulo(9);
         Cuadrado cuadrado1 = new Cuadrado(9);
         Rectangulo rectangulo1 = new Rectangulo(9,17);
         Triangulo triangulo1= new Triangulo(9,17);
-        JOptionPane.showMessageDialog(null, "El area del circulo es: "+circulo1.calcularArea());
+        /*JOptionPane.showMessageDialog(null, "El area del circulo es: "+circulo1.calcularArea());
         JOptionPane.showMessageDialog(null, "El perimetro del circulo es: "+circulo1.calcularPerimetro());
         JOptionPane.showMessageDialog(null, "El area del cuadrado es: "+cuadrado1.calcularArea());
         JOptionPane.showMessageDialog(null, "El perimetro del cuadrado es: "+cuadrado1.calcularPerimetro());
@@ -25,6 +26,19 @@ public class Principal {
         JOptionPane.showMessageDialog(null, "El perimetro del rectangulo es: "+rectangulo1.calcularPerimetro());
         JOptionPane.showMessageDialog(null, "El area del triangulo es: "+triangulo1.calcularArea());
         JOptionPane.showMessageDialog(null, "El perimetro del triangulo es: "+triangulo1.calcularPerimetro());
+        */
+        f=circulo1;
+        System.out.println(f.calcularArea());
+        System.out.println(f.calcularPerimetro());
+        f=triangulo1;
+        System.out.println(f.calcularArea());
+        System.out.println(f.calcularPerimetro());
+        f=rectangulo1;
+        System.out.println(f.calcularArea());
+        System.out.println(f.calcularPerimetro());
+        f=cuadrado1;
+        System.out.println(f.calcularArea());
+        System.out.println(f.calcularPerimetro());
         
     }
 }
